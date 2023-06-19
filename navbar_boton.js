231a575282140d573d5d2fb1269d6575504b8f55
antonieta-p <antonietaparpaglioni@gmail.com>
2023-06-19 00:22:59 -0300

    vr

---
+++ navbar_boton.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    var navbarMenu = document.getElementById("navbarMenu");
+    navbarMenu.style.display = navbarMenu.style.display === "block" ? "none" : "block";
+  }
+  
